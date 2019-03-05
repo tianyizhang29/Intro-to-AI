dc8394098942fefffa16f430a4f43204fa4be74a
ryan <tianyizhang29@gmail.com>
2019-03-04 22:40:30 -0500

    fix all bugs, version 1.0

--- Assignment2/mineSweeper.py
+++ Assignment2/mineSweeper.py
@@ -1,7 +1,9 @@
+
+
 import numpy as np
 import random
 
-class cell:
+class Cell:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -12,7 +14,10 @@ class minSweeper:
         self.grid = np.load(file)
         self.d = len(self.grid)
         self.n = 15
-        self.reveal = [["N" * self.d] in range(self.d)]
+        #self.reveal = [["N" * self.d] in range(self.d)]
+        tenN = ["N", "N", "N", "N", "N", "N", "N", "N", "N", "N"]
+        self.visited = np.reshape(np.zeros(self.d * self.d), (self.d, self.d))
+        self.reveal = [tenN.copy() for i in range(self.d)]
         self.to_be_revealed = []
         self.num_mine = 0
         self.dx = [-1, 0, 1]
@@ -20,7 +25,6 @@ class minSweeper:
 
 
     # 找一定是雷的点，标记
-    # 不是8个点。
     def find_mine(self, cell):
         cur_x = cell.x
         cur_y = cell.y
@@ -29,43 +33,49 @@ class minSweeper:
             for j in range(3):
                 new_x = cur_x + self.dx[i]
                 new_y = cur_y + self.dy[j]
-                if self.reveal[new_x][new_y] == "N" or self.reveal[new_x][new_y] == "*" or self.reveal[new_x][new_y] == "W":
-                    continue
-                else:
-                    neighbour_hint.append(cell(new_x, new_y))
+                if new_x >=0 and new_x < self.d and new_y >= 0 and new_y < self.d:
+                    if self.reveal[new_x][new_y].__eq__("N") or self.reveal[new_x][new_y] .__eq__("*") or self.reveal[new_x][new_y].__eq__("W") :
+                        continue
+                    else:
+                        neighbour_hint.append(Cell(new_x, new_y))
 
-
-
-        # bug -> 多个位置可以判定是雷或者空
         while len(neighbour_hint) != 0:
-            cur_cell = neighbour_hint.pop(0)
+            hint_cell = neighbour_hint.pop(0)
             last_pos = 9
-            mine = self.grid[cur_cell.x][cur_cell.y]
+            mine = self.grid[hint_cell.x][hint_cell.y]
             for i in range(3):
                 for j in range(3):
-                    x = cur_cell.x + self.dx[i]
-                    y = cur_cell.y + self.dy[j]
-                    if self.reveal[x][y] == "N":
-                        continue
-                    elif self.reveal[x][y] == "*" and self.reveal[x][y] == "W":
-                        mine = mine - 1
-                        last_pos -= 1
+                    x = hint_cell.x + self.dx[i]
+                    y = hint_cell.y + self.dy[j]
+                    if x >=0 and x < self.d and y >= 0 and y < self.d:
+                        if last_pos != mine:
+                            if self.reveal[x][y] == "N":
+                                continue
+                            elif self.reveal[x][y] == "*" or self.reveal[x][y] == "W":
+                                mine = mine - 1
+                                last_pos -= 1
+                            else:
+                                last_pos = last_pos - 1
+                        # 一定是雷
+                        if last_pos == mine:
+                            print("(%s, %s) is mine!!!!" % (cell.x, cell.y))
+                            return 1
+                        # 一定不是雷
+                        elif mine == 0 and last_pos != 0:
+                            print("(%s, %s) is safe!!!!" % (cell.x, cell.y))
+                            return 2
                     else:
-                        last_pos = last_pos - 1
-                    # 一定是雷
-                    if last_pos == mine:
-                        return 1
-                    # 一定不是雷
-                    elif mine == 0:
-                        return 2
-            return 0
+                        last_pos -= 1
+        print("(%s, %s) unsure!!!!" % (cell.x, cell.y))
+        return 0
 
     # 当没有能够推论出一定是雷的点的时候，找是雷可能性最小的点。
     def find_reachable_position(self):
         min_prob = 8
         res_index = -1
         for i in range(len(self.to_be_revealed)):
-            prob = self.cal_possibility(self.to_be_revealed.__getitem__(i))
+            cur_cell = self.to_be_revealed.__getitem__(i)
+            prob = self.cal_possibility(cur_cell)
             if prob < min_prob:
                 min_prob = prob
                 res_index = i
@@ -80,22 +90,25 @@ class minSweeper:
                 new_x = cell.x + self.dx[i]
                 new_y = cell.y + self.dy[j]
                 if new_x >=0 and new_x < self.d and new_y >= 0 and new_y < self.d:
-                    if self.grid[new_x][new_y] != "N" or self.grid[new_x][new_y] != "*":
+                    # Find number
+                    if not self.reveal[new_x][new_y].__eq__("N") and not self.reveal[new_x][new_y].__eq__('*'):
                         last_pos = 9
                         mine = self.grid[new_x][new_y]
-                        for i in range(3):
-                            for j in range(3):
-                                x = new_x + self.dx[i]
-                                y = new_y + self.dy[j]
-                                if self.grid[x][y] == "N":
-                                    continue
-                                elif self.grid[x][y] == "*":
-                                    mine = mine - 1
-                                    last_pos -= 1
+                        for m in range(3):
+                            for n in range(3):
+                                x = new_x + self.dx[m]
+                                y = new_y + self.dy[n]
+                                if x >= 0 and x < self.d and y >= 0 and y < self.d:
+                                    if self.reveal[x][y].__eq__("N") :
+                                        continue
+                                    elif self.reveal[x][y].__eq__('*') or self.reveal[x][y].__eq__("W"):
+                                        mine = mine - 1
+                                        last_pos -= 1
+                                    else:
+                                        last_pos = last_pos - 1
                                 else:
-                                    last_pos = last_pos - 1
+                                    last_pos -= 1
                         res += mine / last_pos
-
         return res
 
     # 计算这个点是雷的最大可能性
@@ -113,60 +126,80 @@ class minSweeper:
                 new_x = x + self.dx[i]
                 new_y = y + self.dy[j]
                 if new_x >= 0 and new_x < self.d and new_y >= 0 and new_y < self.d:
-                    if self.grid[new_x][new_y] == "N":
-                        self.to_be_revealed.__add__(cell(new_x, new_y))
+                    if self.reveal[new_x][new_y].__eq__("N") and  self.visited[new_x][new_y] == 0:
+                        self.to_be_revealed.append(Cell(new_x, new_y))
+                        self.visited[new_x][new_y] = 1
 
     def check_finished(self):
-        for i in self.reveal:
-            if i == "N":
-                return False
+        for i in self.reveal.__iter__():
+            for item in i.__iter__():
+                if item == "N":
+                    return False
         return True
-
+    def print_reveal(self):
+        for i in range(self.d):
+            for j in range(self.d):
+                print(self.reveal[i][j], end='  ')
+            print('')
+        print('----------------------------------------')
     # start_game
     def game(self):
         # find a start point to start;
-        x = random.randint(0, self.d)
-        y = random.randint(0, self.d)
+        # x = random.randint(0, self.d - 1)
+        # y = random.randint(0, self.d - 1)
+        x = 5
+        y = 1
         num = self.grid[x][y]
         if (num == -1):
             print("boom!")
             return
         else:
-            self.reveal[x][y] = str(num)
+            self.reveal[x][y] = num
+            self.visited[x][y] = 2
             self.check_neighbour(x, y)
+        self.print_reveal()
         while True:
+            self.print_reveal()
             find_mark = False
             for i in range(len(self.to_be_revealed)):
                 cur_cell = self.to_be_revealed.__getitem__(i)
                 # 这个点一定是雷, Mark, 更新to_be_revealed。Break;
-                if self.find_mine(cur_cell) == 1:
+                check_res = self.find_mine(cur_cell)
+                if check_res == 1:
                     self.num_mine +=1
                     self.reveal[cur_cell.x][cur_cell.y] = "*"
+                    self.visited[cur_cell.x][cur_cell.y] = 2
                     self.check_neighbour(cur_cell.x, cur_cell.y)
                     self.to_be_revealed.pop(i)
                     find_mark = True
                     break
                 # 一定不是雷
-                elif self.find_mine(cur_cell) == 2:
+                elif check_res == 2:
                     self.reveal[cur_cell.x][cur_cell.y] = self.grid[cur_cell.x][cur_cell.y]
+                    self.visited[cur_cell.x][cur_cell.y] = 2
                     self.to_be_revealed.pop(i)
                     self.check_neighbour(cur_cell.x, cur_cell.y)
+                    find_mark = True
                     break
             if find_mark:
                 continue
             # 已经标记了所有是雷的情况：
             else:
-                next_position = self.find_reachable_position()
-                next_cell = self.to_be_revealed.__getitem__(next_position)
-                self.to_be_revealed.pop(next_position)
-                if self.grid[next_cell.x][next_cell.y] == "-1":
-                    self.reveal[next_cell.x][next_cell.y] = "W"
-                else:
-                    self.reveal[next_cell.x][next_cell.y] = self.grid[next_cell.x][next_cell.y]
-                    self.to_be_revealed.append(cell(next_cell))
+                next_index = self.find_reachable_position()
+                if next_index != -1:
+                    next_cell = self.to_be_revealed.__getitem__(next_index)
+                    print("Into Guess Part; Guess(%s, %s) as next cell" % (next_cell.x, next_cell.y))
+                    self.to_be_revealed.pop(next_index)
+                    self.visited[next_cell.x][next_cell.y] = 2
+                    if self.grid[next_cell.x][next_cell.y] == "-1":
+                        self.reveal[next_cell.x][next_cell.y] = "W"
+                    else:
+                        self.reveal[next_cell.x][next_cell.y] = self.grid[next_cell.x][next_cell.y]
+                        self.check_neighbour(next_cell.x, next_cell.y)
             if self.check_finished():
                 print("---------------Finished-------------------")
                 print("correct rate: %s" % (self.num_mine / 15.0))
+                return
 
 
 
